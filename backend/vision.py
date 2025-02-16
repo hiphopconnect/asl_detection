a1f6cd851f4498766f19eb926be25126aa8543ea
jojo2004 <johannes.geiger@hs-augsburg.de>
2025-02-16 22:04:57 +0000

    #18- change loading of mediaPipe Model in __init__

--- backend/vision.py
+++ backend/vision.py
@@ -1,8 +1,10 @@
-import numpy as np
 import cv2
 import mediapipe as mp
+import numpy as np
+
 from backend.algorithms import Algorithm
 from backend.custom_types import AlgorithmType, ModelName
+
 
 class MediaPipeHolistics(Algorithm):
     """
@@ -25,51 +27,47 @@ class MediaPipeHolistics(Algorithm):
         super().__init__()
         self.type = AlgorithmType.POSEDETECTION
         self.name = ModelName.MEDIAPIPE_HOLISTICS
-        self.model = mp.solutions.holistic.Holistic()
-        # mp_drawing = mp.solutions.drawing_utils
-        # mp_drawing_styles = mp.solutions.drawing_styles
-        # mp_holistic = mp.solutions.holistic
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.mp_holistic = mp.solutions.holistic
 
+        self.model = self.mp_holistic.Holistic(
+            min_detection_confidence=0.5, min_tracking_confidence=0.5
+        )
 
     def __call__(self, frame: np.ndarray) -> np.ndarray:
-        mp_drawing = mp.solutions.drawing_utils
-        mp_drawing_styles = mp.solutions.drawing_styles
-        mp_holistic = mp.solutions.holistic
+        frame.flags.writeable = False
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+        results = self.model.process(frame)
 
-        with mp_holistic.Holistic(
-            min_detection_confidence = 0.5,
-            min_tracking_confidence = 0.5) as holistics:
+        frame.flags.writeable = True
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            frame.flags.writeable = False
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = holistics.process(frame)
-            frame.flags.writeable = True
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        if results.face_landmarks:
+            self.mp_drawing.draw_landmarks(
+                frame,
+                results.face_landmarks,
+                self.mp_holistic.FACEMESH_CONTOURS,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=self.mp_drawing_styles.get_default_face_mesh_contours_style(),
+            )
 
-            if results.face_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame,
-                    results.face_landmarks,
-                    mp_holistic.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style()
-                    )
-            
-            if results.pose_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame,
-                    results.pose_landmarks,
-                    mp_holistic.POSE_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-                )
+        if results.pose_landmarks:
+            self.mp_drawing.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                self.mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec=self.mp_drawing_styles.get_default_pose_landmarks_style(),
+            )
 
-            if results.left_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            if results.right_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            
+        if results.left_hand_landmarks:
+            self.mp_drawing.draw_landmarks(
+                frame, results.left_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS
+            )
+        if results.right_hand_landmarks:
+            self.mp_drawing.draw_landmarks(
+                frame, results.right_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS
+            )
+
         return frame
-
